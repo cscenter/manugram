0f13261943359032750bb72da8b8f1cd5619dc7b
Egor Suvorov <egor.suvorov@gmail.com>
2014-11-15 14:47:00 +0300

    Some tests for Segment::distanceToBorder were added

---
+++ tests.cpp
@@ -0,0 +1,23 @@
+#include <QtTest/QtTest>
+#include "model.h"
+
+using namespace figures;
+
+class TestFiguresDistances : public QObject {
+    Q_OBJECT
+private slots:
+    void testDistanceToSegmentBorder() {
+        const double EPS = 1e-8;
+        Segment s(Point(1, 1), Point(4, 4));
+        QVERIFY(fabs(s.getDistanceToBorder(Point(1, 1)) - 0) < EPS);
+        QVERIFY(fabs(s.getDistanceToBorder(Point(1, 0)) - 1) < EPS);
+        QVERIFY(fabs(s.getDistanceToBorder(Point(0, 0)) - sqrt(2)) < EPS);
+        QVERIFY(fabs(s.getDistanceToBorder(Point(4, 4)) - 0) < EPS);
+        QVERIFY(fabs(s.getDistanceToBorder(Point(5, 4)) - 1) < EPS);
+        QVERIFY(fabs(s.getDistanceToBorder(Point(5, 5)) - sqrt(2)) < EPS);
+        QVERIFY(fabs(s.getDistanceToBorder(Point(2, 2)) - 0) < EPS);
+        QVERIFY(fabs(s.getDistanceToBorder(Point(4, 2)) - sqrt(2)) < EPS);
+    }
+};
+
+//QTEST_MAIN(TestFiguresDistances)
