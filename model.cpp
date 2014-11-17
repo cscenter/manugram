f305580c9c658cdada04dacd60c54ed53ab9aac8
Egor Suvorov <egor.suvorov@gmail.com>
2014-11-17 14:30:31 +0300

    model.h: big getDistanceTo* methods were moved to *.cpp (#10)

--- model.cpp
+++ model.cpp
@@ -1,6 +1,52 @@
 #include "model.h"
 #include <string>
 
+double figures::Segment::getDistanceToBorder(const Point &p) {
+    double res = std::min((p - a).length(), (p - b).length());
+    // Please note that here we do not care about floatint-point error,
+    // as if 'start' falls near 'a' or 'b', we've already calculated it
+    if (Point::dotProduct(p - a, b - a) >= 0
+            && Point::dotProduct(p - b, a - b) >= 0) {
+        res = std::min(res, getDistanceToLine(p));
+    }
+    return res;
+}
+
+double figures::Segment::getDistanceToLine(const Point &p) {
+    // Coefficients of A*x + B*y + C = 0
+    double A = b.y - a.y;
+    double B = a.x - b.x;
+    double C = -A * a.x - B * a.y;
+
+    // normalization of the equation
+    double D = sqrt(A * A + B * B);
+    if (fabs(D) < 1e-8) { return HUGE_VAL; } // degenerate case
+
+    A /= D; B /= D; C /= D;
+    return fabs(A * p.x + B * p.y + C);
+}
+
+double figures::Rectangle::getDistanceToBorder(const Point &p) {
+    bool inX = box.leftDown.x <= p.x && p.x <= box.rightUp.x;
+    bool inY = box.leftDown.y <= p.y && p.y <= box.rightUp.y;
+    double res = HUGE_VAL;
+    if (inX) { // Point lies in a vertical bar bounded by Left and Right
+        res = std::min(res, fabs(p.y - box.leftDown.y));
+        res = std::min(res, fabs(p.y - box.rightUp.y));
+    }
+    if (inY) { // Point lies in a horizontal bar
+        res = std::min(res, fabs(p.x - box.leftDown.x));
+        res = std::min(res, fabs(p.x - box.rightUp.x));
+    }
+    if (!inX && !inY) {
+        res = std::min(res, (p - box.leftDown).length());
+        res = std::min(res, (p - box.rightUp).length());
+        res = std::min(res, (p - box.leftUp()).length());
+        res = std::min(res, (p - box.rightDown()).length());
+    }
+    return res;
+}
+
 std::istream &operator>>(std::istream &in, Model &model) {
     int count;
     if (!(in >> count)) {
--- model.h
+++ model.h
@@ -95,33 +95,12 @@ public:
     Point getA() const { return a; }
     Point getB() const { return b; }
 
-    double getDistanceToBorder(const Point &p) override {
-        double res = std::min((p - a).length(), (p - b).length());
-        // Please note that here we do not care about floatint-point error,
-        // as if 'start' falls near 'a' or 'b', we've already calculated it
-        if (Point::dotProduct(p - a, b - a) >= 0
-                && Point::dotProduct(p - b, a - b) >= 0) {
-            res = std::min(res, getDistanceToLine(p));
-        }
-        return res;
-    }
+    double getDistanceToBorder(const Point &p) override;
 
 private:
     Point a, b;
 
-    double getDistanceToLine(const Point &p) {
-        // Coefficients of A*x + B*y + C = 0
-        double A = b.y - a.y;
-        double B = a.x - b.x;
-        double C = -A * a.x - B * a.y;
-
-        // normalization of the equation
-        double D = sqrt(A * A + B * B);
-        if (fabs(D) < 1e-8) { return HUGE_VAL; } // degenerate case
-
-        A /= D; B /= D; C /= D;
-        return fabs(A * p.x + B * p.y + C);
-    }
+    double getDistanceToLine(const Point &p);
 };
 
 class BoundedFigure : public Figure {
@@ -161,26 +140,7 @@ public:
         res << "rectangle(" << getBoundingBox().leftDown.str() << "--" << getBoundingBox().rightUp.str() << ")";
         return res.str();
     }
-    double getDistanceToBorder(const Point &p) override {
-        bool inX = box.leftDown.x <= p.x && p.x <= box.rightUp.x;
-        bool inY = box.leftDown.y <= p.y && p.y <= box.rightUp.y;
-        double res = HUGE_VAL;
-        if (inX) { // Point lies in a vertical bar bounded by Left and Right
-            res = std::min(res, fabs(p.y - box.leftDown.y));
-            res = std::min(res, fabs(p.y - box.rightUp.y));
-        }
-        if (inY) { // Point lies in a horizontal bar
-            res = std::min(res, fabs(p.x - box.leftDown.x));
-            res = std::min(res, fabs(p.x - box.rightUp.x));
-        }
-        if (!inX && !inY) {
-            res = std::min(res, (p - box.leftDown).length());
-            res = std::min(res, (p - box.rightUp).length());
-            res = std::min(res, (p - box.leftUp()).length());
-            res = std::min(res, (p - box.rightDown()).length());
-        }
-        return res;
-    }
+    double getDistanceToBorder(const Point &p) override;
 };
 } // namespace figures
 
