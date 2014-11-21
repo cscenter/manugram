03f4c89ed10e89a0b27bed404b3e2614bffe18ef
Egor Suvorov <egor.suvorov@gmail.com>
2014-11-21 16:30:20 +0300

    testDistanceToSegmentBorder(): slightly better test

--- tests.cpp
+++ tests.cpp
@@ -8,15 +8,15 @@ class TestFiguresDistances : public QObject {
 private slots:
     void testDistanceToSegmentBorder() {
         const double EPS = 1e-8;
-        Segment s(Point(1, 1), Point(4, 4));
-        QVERIFY(fabs(s.getDistanceToBorder(Point(1, 1)) - 0) < EPS);
-        QVERIFY(fabs(s.getDistanceToBorder(Point(1, 0)) - 1) < EPS);
-        QVERIFY(fabs(s.getDistanceToBorder(Point(0, 0)) - sqrt(2)) < EPS);
-        QVERIFY(fabs(s.getDistanceToBorder(Point(4, 4)) - 0) < EPS);
-        QVERIFY(fabs(s.getDistanceToBorder(Point(5, 4)) - 1) < EPS);
-        QVERIFY(fabs(s.getDistanceToBorder(Point(5, 5)) - sqrt(2)) < EPS);
-        QVERIFY(fabs(s.getDistanceToBorder(Point(2, 2)) - 0) < EPS);
-        QVERIFY(fabs(s.getDistanceToBorder(Point(4, 2)) - sqrt(2)) < EPS);
+        Segment s(Point(11, 1), Point(14, 4));
+        QVERIFY(fabs(s.getDistanceToBorder(Point(11, 1)) - 0) < EPS);
+        QVERIFY(fabs(s.getDistanceToBorder(Point(11, 0)) - 1) < EPS);
+        QVERIFY(fabs(s.getDistanceToBorder(Point(10, 0)) - sqrt(2)) < EPS);
+        QVERIFY(fabs(s.getDistanceToBorder(Point(14, 4)) - 0) < EPS);
+        QVERIFY(fabs(s.getDistanceToBorder(Point(15, 4)) - 1) < EPS);
+        QVERIFY(fabs(s.getDistanceToBorder(Point(15, 5)) - sqrt(2)) < EPS);
+        QVERIFY(fabs(s.getDistanceToBorder(Point(12, 2)) - 0) < EPS);
+        QVERIFY(fabs(s.getDistanceToBorder(Point(14, 2)) - sqrt(2)) < EPS);
     }
 };
 
