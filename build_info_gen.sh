acaab60e1177b7bef64cba1eac0c70b24827a388
Egor Suvorov <egor.suvorov@gmail.com>
2015-04-11 16:26:40 +0300

    build_info_gen: now full git hash is displayed (#62)

--- build_info_gen.sh
+++ build_info_gen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pushd $1 >/dev/null
-GIT_SHA1=$(git log -1 --pretty=%h)
+GIT_SHA1=$(git log -1 --pretty=%H)
 GIT_TIME=$(git log -1 --pretty=%ad --date=iso || exit 1)
 TIME=$(date +'%Y-%m-%d %H:%M:%S %z')
 popd >/dev/null
