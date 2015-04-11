83e201a9d912e079db4bcdeb1a7e9699b1106ad7
Egor Suvorov <egor.suvorov@gmail.com>
2015-04-11 16:25:57 +0300

    build_info: build time was added (#62)

--- build_info.h
+++ build_info.h
@@ -3,6 +3,7 @@
 
 extern const char *GIT_LAST_SHA1;
 extern const char *GIT_LAST_TIME;
+extern const char *BUILD_TIME;
 
 #endif // BUILD_INFO
 
--- build_info_gen.sh
+++ build_info_gen.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 pushd $1 >/dev/null
-SHA1=$(git log -1 --pretty=%h)
-TIME=$(git log -1 --pretty=%ad --date=iso || exit 1)
-popd 2>&1
-if [ "$SHA1" == "" ]; then exit 1; fi
+GIT_SHA1=$(git log -1 --pretty=%h)
+GIT_TIME=$(git log -1 --pretty=%ad --date=iso || exit 1)
+TIME=$(date +'%Y-%m-%d %H:%M:%S %z')
+popd >/dev/null
+if [ "$GIT_SHA1" == "" ]; then exit 1; fi
+
 function gen {
   echo "#include \"build_info.h\""
-  echo "const char *GIT_LAST_SHA1 = \"$SHA1\";"
-  echo "const char *GIT_LAST_TIME = \"$TIME\";"
+  echo "const char *GIT_LAST_SHA1 = \"$GIT_SHA1\";"
+  echo "const char *GIT_LAST_TIME = \"$GIT_TIME\";"
+  echo "const char *BUILD_TIME = \"$TIME\";"
 }
-gen >build_info.cpp 2>&1
+gen >build_info.cpp
